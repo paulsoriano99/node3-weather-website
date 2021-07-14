83cc4bf8b065862837e917cbbc46e079cf9aeaa1
Paul Soriano <aldwinsoriano17@gmail.com>
2021-07-15 01:25:13 +0800

    add high and low

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,7 @@ const forecast = (latitude, longtitude, callback) => {
             callback('Unable to find location', undefined)
         }
         else {
-            callback(undefined + body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + 'degrees out. Humidity is ' + body.current.humidity + '%')
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + 'degrees out. Humidity is ' + body.current.humidity + '%')
         }
     })
 }
